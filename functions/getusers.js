509b59b9cf9b22aa68e84815078f8713fce05ad5
satish-rajnale <satishrajnale98@gmail.com>
2021-04-25 23:11:56 +0530

    netlify build

--- functions/getusers.js
+++ functions/getusers.js
@@ -3,9 +3,10 @@ const axios = require('axios');
 
 
 exports.handler = function(event, context, callback){
-    const API_URL = 'https://api.github.com/users'
-    const API_CLIENT_ID =  "efacf249eaa1cb0ce7c5";
-    const API_CLIENT_SECRET = "55ffa817809af2f871322e9c0feaf4a2131ab904";
+    const {API_URL, API_CLIENT_ID,  API_CLIENT_SECRET} = process.env;
+    // const API_URL = 'https://api.github.com/users'
+    // const API_CLIENT_ID =  "efacf249eaa1cb0ce7c5";
+    // const API_CLIENT_SECRET = "55ffa817809af2f871322e9c0feaf4a2131ab904";
 
     const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`;
 
