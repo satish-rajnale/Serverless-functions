1d67d3356221bbb03a9be4cb0d35a4907edfbc75
satish-rajnale <satishrajnale98@gmail.com>
2021-04-25 23:27:49 +0530

    funcSafe

---
+++ funcSafe/functions/getusers.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+
+
+
+exports.handler = function(event, context, callback){
+    const {API_URL, API_CLIENT_ID,  API_CLIENT_SECRET} = process.env;
+    // const API_URL = 'https://api.github.com/users'
+    // const API_CLIENT_ID =  "efacf249eaa1cb0ce7c5";
+    // const API_CLIENT_SECRET = "55ffa817809af2f871322e9c0feaf4a2131ab904";
+
+    const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`;
+
+    //Send user response
+    const send = (body) =>{
+        callback(null, {
+            statusCode : 200,
+            body : JSON.stringify(body),
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept',
+            }
+        })
+    }
+
+    //perform an api call
+    const getUsers = () => {
+        axios.get(URL)
+            .then(res => send(res.data))
+            .catch(err => send(err))
+    }
+
+    // Make sure call is a GET
+    if(event.httpMethod == "GET"){
+        getUsers();
+    }
+
+    // const {name} = JSON.parse(event.body); 
+    // callback(null, {
+    //     statusCode : 200,
+    //     body : JSON.stringify({ msg :"Hello "+ name }) 
+    // })
+}
